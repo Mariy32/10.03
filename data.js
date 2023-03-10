ca73d697115c614ab5f1acd46884451f22cff32f
Mariy32 <zyabliceva.mariy@bk.ru>
2023-03-10 17:26:02 +0300

    09.03

---
+++ data.js
@@ -0,0 +1,77 @@
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+
+// Задание 2
+// 1. Создать файл index.html.
+// 2. Подключить data.js.
+// 3. Сформировать контент из данных (картинка загловок и параграф).
+// 4. Добавить данный контент в вёрстку.
+// 5. * Добавить стили при необходимости (по желанию).
+
+
+const dataInfo = `[
+{
+  "activity": "Plan a vacation you've always wanted to take",
+  "type": "relaxation",
+  "participants": 1,
+  "price": 0,
+  "link": "",
+  "key": "7265395",
+  "accessibility": 0.05,
+  "link": "https://melnikov-art32.ru/wp-content/uploads/2020/05/pejzazh-gory-melnikov.jpg",
+  "img": "https://melnikov-art32.ru/wp-content/uploads/2020/05/pejzazh-gory-melnikov.jpg"
+},
+{
+	"activity": "Learn a new programming language",
+	"accessibility": 0.25,
+	"type": "education",
+	"participants": 1,
+	"price": 0.1,
+	"key": "5881028",
+    "link": "https://www.vinterier.ru/pictures/shop/krasivyiy-peiyzag-kartina-maslom-40x30_x500.jpg",
+    "img": "https://www.vinterier.ru/pictures/shop/krasivyiy-peiyzag-kartina-maslom-40x30_x500.jpg"
+},
+{
+	"activity": "Learn how to play a new sport",
+	"accessibility": 0.2,
+	"type": "recreational",
+	"participants": 1,
+	"price": 0.1,
+	"key": "5808228",
+    "link": "https://www.vinterier.ru/pictures/shop/gornyiy-peiyzag-kartina-maslom-70x50_x500.jpg",
+    "img": "https://www.vinterier.ru/pictures/shop/gornyiy-peiyzag-kartina-maslom-70x50_x500.jpg"
+
+},
+{
+	"activity": "Learn how to fold a paper crane",
+	"accessibility": 0.05,
+	"type": "education",
+	"participants": 1,
+	"price": 0.1,
+	"key": "3136036",
+    "link": "https://arthive.net/res/media/img/oy800/work/68f/541348@2x.webp",
+    "img": "https://arthive.net/res/media/img/oy800/work/68f/541348@2x.webp"
+},
+{
+	"activity": "Hold a yard sale",
+	"accessibility": 0.1,
+	"type": "social",
+	"participants": 1,
+	"price": 0.0,
+    "link": "https://arthive.net/res/media/img/oy800/work/7be/541731@2x.webp",
+    "img": "https://arthive.net/res/media/img/oy800/work/7be/541731@2x.webp"
+
+},
+{
+	"activity": "Teach your dog a new trick",
+	"accessibility": 0.15,
+	"type": "relaxation",
+	"participants": 1,
+	"price":0.05,
+    "link": "https://arthive.net/res/media/img/oy800/ref/224/280235@2x.webp",
+    "img": "https://arthive.net/res/media/img/oy800/ref/224/280235@2x.webp"
+}
+]`;
